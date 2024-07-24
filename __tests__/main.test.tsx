be6076f6f270277c8b331497c5b4de280bb3c245
bbbjihan <parkjihan.dev@gmail.com>
2024-07-25 01:18:47 +0900

    테스트 라이브러리 추가 및 ci 스크립트 일부 수정

---
+++ __tests__/main.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import TestPage from "../src/app/testpage/page";
+
+describe("샘플 테스트", () => {
+  test("렌더링 테스트", () => {
+    render(<TestPage />);
+    const $page = screen.getByTestId("test_div");
+
+    expect($page.innerHTML.indexOf("TestPage")).not.toBe(-1);
+  });
+});
---
+++ src/app/testpage/page.tsx
@@ -0,0 +1,5 @@
+const TestPage = () => {
+  return <div data-testid="test_div">TestPage</div>;
+};
+
+export default TestPage;
